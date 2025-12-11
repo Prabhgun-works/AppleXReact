b29e2453581f8edc356648050549f7559b60441b
Prabhgun Singh <prabhgun.web@gmail.com>
2025-12-11 17:52:25 +0530

    Optimize src/App.jsx and format code

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import Hero from "./components/hero";
 import ProductViewer from "./components/productViewer";
 import Showcase from "./components/showcase";
 import Performance from "./components/performance";
-import gsap from 'gsap';
+import gsap from "gsap";
 import Features from "./components/features";
 import Highlights from "./components/highlights";
-import { ScrollTrigger , SplitText} from "gsap/all";
+import Footer from "./components/footer";
+import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
 export default function App() {
-    return(
-        <main>
-            <Navbar/>
-            <Hero/>
-            <ProductViewer/>
-            <Showcase/>
-            <Performance/>
-            <Features/>
-            <Highlights/>
-            <Footer/>
-        </main>
-    )
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
+    </main>
+  );
 }
